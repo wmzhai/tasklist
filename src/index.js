cc38d6375c9781e38ddb3b570ab6c6df35180763
Weiming Zhai <wmzhai@gmail.com>
2017-03-28 14:28:48 +0800

    Build the app using React Router v4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './screens/app';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const { REACT_APP_API_HOST = 'http://localhost:3000' } = process.env;
+
+const networkInterface = createNetworkInterface({
+  uri: `${REACT_APP_API_HOST}/graphql`,
+});
+
+const client = new ApolloClient({ networkInterface });
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </ApolloProvider>
+), document.getElementById('root'));
